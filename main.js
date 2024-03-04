75fb944502d27213af38e8c027518dbaa5f19004
ONENEN11 <161000890+ONENEN11@users.noreply.github.com>
2024-03-04 22:15:57 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("This is a responsive page designed by Ahmed Salah El-Din, uploaded to Github and still under development.")
+console.log("This is a responsive page designed by Ahmed Salah El-Din, uploaded to Github and still under development.:copyright@shalaboka")
